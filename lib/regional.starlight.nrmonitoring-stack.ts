e688099c512de2d1a5eed1c472b35b25c6c08a5e
David Ward <david.ward@sage.com>
2022-04-28 11:40:50 +0100

    Added basic infra to cdk file

--- lib/regional.starlight.nrmonitoring-stack.ts
+++ lib/regional.starlight.nrmonitoring-stack.ts
@@ -1,16 +1,83 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps, Duration } from "aws-cdk-lib";
+import { Construct, Node } from "constructs";
+import { Architecture, Code, Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import * as apig from "aws-cdk-lib/aws-apigateway";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as sfn from "aws-cdk-lib/aws-stepfunctions";
+import * as tasks from "aws-cdk-lib/aws-stepfunctions-tasks";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class RegionalStarlightNrmonitoringStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    // Create lambdas
+    const test = new NodejsFunction(this, "test test", {
+      functionName: "test1",
+      runtime: Runtime.NODEJS_14_X,
+      entry: "functions/test.js",
+      logRetention: RetentionDays.ONE_WEEK,
+      architecture: Architecture.ARM_64,
+      memorySize: 1024,
+    });
 
-    // The code that defines your stack goes here
+    // Create the workflow
+    const getTest = new tasks.LambdaInvoke(this, "Test", {
+      lambdaFunction: test,
+      outputPath: "$.Payload",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'RegionalStarlightNrmonitoringQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const wait = new sfn.Wait(this, "Wait", {
+      time: sfn.WaitTime.duration(Duration.seconds(3)),
+    });
+
+    const definition = getTest.next(wait);
+
+    const stateMachine = new sfn.StateMachine(
+      this,
+      "Automated-monitoring-New-Relic",
+      {
+        definition,
+        timeout: Duration.minutes(3),
+      }
+    )
+
+    const sfnArn = stateMachine.stateMachineArn;
+
+    // Create the API
+    const apigateway = new apig.RestApi(this, "endpoint");
+
+    const credentialsRole = new iam.Role(this, "getRole", {
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com"),
+    });
+
+    apigateway.root.addMethod(
+      "POST",
+      new apig.AwsIntegration({
+        service: "states",
+        action: "StartExecution",
+        integrationHttpMethod: "POST",
+        options: {
+          credentialsRole,
+          integrationResponses: [
+            {
+              statusCode: "200",
+              responseTemplates: {
+                "application/json": `{"complete": true}`,
+              },
+            },
+          ],
+          requestTemplates: {
+            "application/json": `{
+              "stateMachineArn": ${sfnArn}
+            }`
+          },
+        },
+      }),
+      {
+        methodResponses: [{ statusCode: "200" }],
+      }
+    );
   }
 }
