88ca31076007c2d7d86e2b048995b6012a86589f
David Ward <david.ward@sage.com>
2022-05-04 14:26:35 +0100

    Fixed bug

--- functions/initNewRelicMonitoring.js
+++ functions/initNewRelicMonitoring.js
@@ -3,11 +3,11 @@ const { Console } = require("console");
 exports.handler = async (event) => {
     console.log(event);
 
-    const API_KEY = event.apiKey,
-    const NR_ACCOUNT_ID = event.newRelicAccountId,
-    const TEAM_NAME = event.teamName,
-    const PAGERDUTY_KEY = event.pagerdutyApiKey,
-    var RESULT = {},
+    var API_KEY = event.apiKey;
+    var NR_ACCOUNT_ID = event.newRelicAccountId;
+    var TEAM_NAME = event.teamName;
+    var PAGERDUTY_KEY = event.pagerdutyApiKey;
+    var RESULT = {};
 
     if (API_KEY && NR_ACCOUNT_ID && TEAM_NAME && PAGERDUTY_KEY) {
         console.log('All params present and accounted for');
