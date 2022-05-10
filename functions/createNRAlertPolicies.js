5dacc0a70c2ed066187d63332439b65f59838299
David Ward <david.ward@sage.com>
2022-05-10 09:26:55 +0100

    Fleshed out createPolicy lambda

--- functions/createNRAlertPolicies.js
+++ functions/createNRAlertPolicies.js
@@ -1,4 +1,47 @@
+const axios = require('axios');
+const gql = require('graphql-tag');
+const graphql = require('graphql');
+const { print } = graphql;
+
 exports.handler = async (event) => {
     console.log('In the create alert policies lambda');
     console.log(event);
+
+    const NR_HOST = "https://api.newrelic.com/graphql";
+
+    const createPolicies = gql`
+        mutation: {
+            _a1: alertPolicyCreate(accountId: ${event.body.NR_ACCOUNT_ID}, policy: {incidentPreference: PER_POLICY, name: "${event.body.TEAM_NAME}"}) {
+                name
+                id
+                incidentPreference
+            }
+        }
+    `,
+
+    try {
+        const graphqlData = await axios({
+            url: NR_HOST,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "API-KEY": event.body.API_KEY
+            },
+            data: {
+                mutation: print(createPolicies)
+            }
+        });
+        const body = {
+            graphqlData: graphqlData.data.data.createPolicies
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(body),
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            }
+        }
+    } catch (err) {
+        console.log('error posting to the API ', err)
+    }
 }
