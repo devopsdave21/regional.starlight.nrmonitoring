145e6600ebbc876201e94a175edaf0a10a410e6b
David Ward <david.ward@sage.com>
2022-05-11 12:17:29 +0100

    Removed brackets

--- functions/createNRAlertPolicies.js
+++ functions/createNRAlertPolicies.js
@@ -1,15 +1,15 @@
-const axios = require('axios');
-const gql = require('graphql-tag');
-const graphql = require('graphql');
+const axios = require("axios");
+const gql = require("graphql-tag");
+const graphql = require("graphql");
 const { print } = graphql;
 
 exports.handler = async (event) => {
-    console.log('In the create alert policies lambda');
-    console.log(event);
+  console.log("In the create alert policies lambda");
+  console.log(event);
 
-    const NR_HOST = "https://api.newrelic.com/graphql";
+  const NR_HOST = "https://api.newrelic.com/graphql";
 
-    const createPolicies = gql `
+  const createPolicies = gql`
     mutation {
         _a1: alertsPolicyCreate(accountId: ${event.body.NR_ACCOUNT_ID}, policy: {incidentPreference: PER_POLICY, name: "${event.body.TEAM_NAME}"}) {
           name
@@ -19,29 +19,27 @@ exports.handler = async (event) => {
       }
     `;
 
-    try {
-        const graphqlData = await axios({
-            url: NR_HOST,
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "API-KEY": event.body.API_KEY
-            },
-            data: {
-                print(createPolicies)
-            }
-        });
-        const body = {
-            graphqlData: graphqlData.data.data.createPolicies
-        }
-        return {
-            statusCode: 200,
-            body: JSON.stringify(body),
-            headers: {
-                "Access-Control-Allow-Origin": "*"
-            }
-        }
-    } catch (err) {
-        console.log('error posting to the API ', err)
-    }
-}
+  try {
+    const graphqlData = await axios({
+      url: NR_HOST,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "API-KEY": event.body.API_KEY,
+      },
+      data: print(createPolicies),
+    });
+    const body = {
+      graphqlData: graphqlData.data.data.createPolicies,
+    };
+    return {
+      statusCode: 200,
+      body: JSON.stringify(body),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+  } catch (err) {
+    console.log("error posting to the API ", err);
+  }
+};
