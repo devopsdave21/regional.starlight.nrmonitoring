bef56705ba8a4cb87030590847c06426f34b14e0
David Ward <david.ward@sage.com>
2022-07-07 12:51:05 +0100

    Custom integration lambda with apig

--- functions/workers/getServicesWorker.js
+++ functions/workers/getServicesWorker.js
@@ -1,30 +1,34 @@
 exports.handler = async (event) => {
-  var data = event.body.data;
-  console.log(event.resource);
-  //console.log(event.body.data.apiKey);
-  console.log(
-    "Parsing input for services to invoke correct step function",
-    data
-  );
+//   var data = event.body.data;
+//   console.log(event.resource);
+//   //console.log(event.body.data.apiKey);
+//   console.log(
+//     "Parsing input for services to invoke correct step function",
+//     data
+//   );
 
-  const variable = event.body.data;
+//   const variable = event.body.data;
 
-if (
-  typeof variable === 'object' &&
-  variable !== null &&
-  !Array.isArray(variable)
-) {
-  console.log('✅ Value is an object');
-} else {
-  console.log('⛔️ Value is not an object');
-}
+// if (
+//   typeof variable === 'object' &&
+//   variable !== null &&
+//   !Array.isArray(variable)
+// ) {
+//   console.log('✅ Value is an object');
+// } else {
+//   console.log('⛔️ Value is not an object');
+// }
+
+let apiKey = event.data.apiKey;
+
+console.log(apiKey);
 
   const response = {
     statusCode: 233,
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
-    body: JSON.stringify(event.body),
+    body: JSON.stringify(apiKey),
     isBase64Encoded: false,
   };
 
