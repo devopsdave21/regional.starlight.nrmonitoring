79d829178fa3046769d68f04f29d02b626e6553b
David Ward <david.ward@sage.com>
2022-07-07 09:26:23 +0100

    Added response to lambda to stop 502 return

--- functions/workers/getServicesWorker.js
+++ functions/workers/getServicesWorker.js
@@ -1,3 +1,17 @@
 exports.handler = async (event) => {
-    console.log('Parsing input for services to invoke correct step function', event.body);
+  console.log(
+    "Parsing input for services to invoke correct step function",
+    event.body.data
+  );
+
+  const response = {
+    statusCode: 233,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(event.body.data),
+    isBase64Encoded: false,
   };
+
+  return response;
+};
