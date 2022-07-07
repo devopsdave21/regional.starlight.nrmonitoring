cbf9e8d435343930fb8bbd10f476a6724e31ef73
David Ward <david.ward@sage.com>
2022-07-07 10:34:00 +0100

    Test

--- functions/workers/getServicesWorker.js
+++ functions/workers/getServicesWorker.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-  var data = event.body.trim();
+  var data = event.body.data.trim();
   console.log(
     "Parsing input for services to invoke correct step function",
     JSON.parse(data)
