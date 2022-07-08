b240442a559b89415067abc8736f047573afd82e
David Ward <david.ward@sage.com>
2022-07-08 13:00:19 +0100

    Parsing services array

--- functions/workers/getServicesWorker.js
+++ functions/workers/getServicesWorker.js
@@ -1,14 +1,21 @@
 exports.handler = async (event) => {
-//var resources = event.data.awsResources;
-console.log(event);
-let result = JSON.parse(event.body);
-console.log(result.data.apiKey);
+  console.log(event);
+  let result = JSON.parse(event.body);
+  console.log(result.data.apiKey);
+
+  // Parse awsResources to invoke. Need to limit on client-side tech stack.
+
+  console.log(`Services included in payload are ${result.data.awsResources}`);
+  var services = result.data.awsResources;
+  services.forEach((s) => {
+    console.log(s);
+  });
 
   const response = {
-    statusCode: 233,
+    statusCode: 200,
     headers: {
       "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(result),
     isBase64Encoded: false,
