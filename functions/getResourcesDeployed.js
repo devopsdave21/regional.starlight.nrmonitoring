fa76ad81c0b525cffc15aa4d4f6e99fcf1a4df6b
David Ward <david.ward@sage.com>
2022-07-12 11:36:34 +0100

    Fixed function file

--- functions/getResourcesDeployed.js
+++ functions/getResourcesDeployed.js
@@ -1,15 +1,21 @@
 exports.handler = async (event) => {
-    console.log('Querying New Relic for resources to get entity guids to use in mutations...')
+  console.log(
+    "Querying New Relic for resources to get entity guids to use in mutations..."
+  );
 
-    try {
-        console.log('Querying for resources now...')
-    } catch (err) {
-        console.log('There was an error: ', err);
-        return {
-            statusCode: 500,
-            body: event,
-        }
-    }
+  try {
+    console.log("Querying for resources now...");
+  } catch (err) {
+    console.log("There was an error: ", err);
+    return {
+      statusCode: 500,
+      body: event,
+    };
+  }
 
-    return event,
-}
+  return {
+    statusCode: 200,
+    body: event,
+    headers: { "Content-Type": "text/plain " },
+  };
+};
