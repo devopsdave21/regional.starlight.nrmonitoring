e108d0cff823ac0d9229dc1c4285975f86ab53fe
David Ward <david.ward@sage.com>
2022-07-12 15:42:04 +0100

    Moved first ECS cluster call into seperate function

--- functions/getResourcesDeployed.js
+++ functions/getResourcesDeployed.js
@@ -3,12 +3,12 @@ const gql = require("graphql-tag");
 const graphql = require("graphql");
 import { NEW_RELIC_URL } from "./constants";
 
-exports.handler = async (event) => {
+const queryForResources = async (obj) => {
   console.log(
-    "Querying New Relic for resources to get entity guids to use in mutations...",
-    JSON.stringify(event)
+    `Querying NR account for resources deployed for account ID ${obj.accountId}`
   );
-
+  // ECS Clusters resources deployed in NR
+  // TODO - Need to put some logic in here to cater for multiple cluster under same account ID
   try {
     console.log("Querying for resources now...");
     const getResources = await axios({
@@ -16,30 +16,30 @@ exports.handler = async (event) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "API-KEY": event.body.event.event.event.RESULT.API_KEY,
+        "API-KEY": obj.accountId,
       },
       data: {
         query: `query {
-            actor {
-                entitySearch(queryBuilder: {infrastructureIntegrationType: AWS_ECS_CLUSTER}) {
-                  count
-                  query
-                  results {
-                    nextCursor
-                    entities {
-                      accountId
-                      entityType
-                      guid
-                      name
-                      type
-                      account {
-                        id
+                actor {
+                    entitySearch(queryBuilder: {infrastructureIntegrationType: AWS_ECS_CLUSTER}) {
+                      count
+                      query
+                      results {
+                        nextCursor
+                        entities {
+                          accountId
+                          entityType
+                          guid
+                          name
+                          type
+                          account {
+                            id
+                          }
+                        }
                       }
                     }
                   }
-                }
-              }
-        }`,
+            }`,
       },
     });
     const response = getResources.data;
@@ -58,4 +58,16 @@ exports.handler = async (event) => {
       body: event,
     };
   }
+};
+
+exports.handler = async (event) => {
+  console.log(
+    "Querying New Relic for resources to get entity guids to use in mutations...",
+    JSON.stringify(event)
+  );
+  const result = await queryForResources({
+    accountId: event.body.event.event.event.RESULT.NR_ACCOUNT_ID,
+    NrAPIKey: event.body.event.event.event.RESULT.API_KEY,
+  });
+  console.log(result);
 };
