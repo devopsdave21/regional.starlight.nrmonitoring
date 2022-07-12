d6fa3b6014b3d411308e7dfb8bd11b0b0a0c2eeb
David Ward <david.ward@sage.com>
2022-07-12 13:07:36 +0100

    Entity sarch fro ECS cluster as test

--- functions/getResourcesDeployed.js
+++ functions/getResourcesDeployed.js
@@ -1,3 +1,8 @@
+const { default: axios } = require("axios");
+const gql = require("graphql-tag");
+const graphql = require("graphql");
+import { NEW_RERLIC_URL } from "./constants";
+
 exports.handler = async (event) => {
   console.log(
     "Querying New Relic for resources to get entity guids to use in mutations..."
@@ -5,6 +10,48 @@ exports.handler = async (event) => {
 
   try {
     console.log("Querying for resources now...");
+    const getResources = await axios({
+      url: NEW_RERLIC_URL,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "API-KEY": body.event.event.RESULT.API_KEY,
+      },
+      data: {
+        query: `mutation {
+                _a1: {
+                    actor {
+                      entitySearch(queryBuilder: {infrastructureIntegrationType: AWS_ECS_CLUSTER}) {
+                        count
+                        query
+                        results {
+                          nextCursor
+                          entities {
+                            accountId
+                            entityType
+                            guid
+                            name
+                            type
+                            account {
+                              id
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+            }`,
+      },
+    });
+    const response = getResources.data;
+    console.log(
+      `The response returned from GQL was ${JSON.stringify(response)}`
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+      headers: { "Content-Type": "text/plain " },
+    };
   } catch (err) {
     console.log("There was an error: ", err);
     return {
@@ -12,10 +59,4 @@ exports.handler = async (event) => {
       body: event,
     };
   }
-
-  return {
-    statusCode: 200,
-    body: event,
-    headers: { "Content-Type": "text/plain " },
-  };
 };
