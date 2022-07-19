a61dd10ea1eb39da77f99d6202652d6cdd9e557c
David Ward <david.ward@sage.com>
2022-07-19 10:48:41 +0100

    Fixed if statement

--- functions/createNRAlertPolicies.js
+++ functions/createNRAlertPolicies.js
@@ -68,7 +68,7 @@ exports.handler = async (event) => {
   TODO - Need to put some logic in here to check if an alert policy has already been created
   with this name
   */
-  if ((hasAlertPolicy = false)) {
+  if ((hasAlertPolicy === false)) {
     try {
       const graphqlData = await axios({
         url: NR_HOST,
